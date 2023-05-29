d6e264477b028378d43945da4731cc4bc67a0396
Oliver <domolitom@gmail.com>
2023-05-29 20:38:47 +0200

    working on parser

--- dev/bin/parser.ml
+++ dev/bin/parser.ml
@@ -8,8 +8,8 @@ exception NotImplemented
 (* Define types needed  *)
 
 type val =
-    int
-  | bool
+    Int of int
+  | Bool of bool
 
 type operator =
     Plus
@@ -17,15 +17,43 @@ type operator =
   | Mult
 
 type expression = 
-    ValExp of val (* value *)
+  | ValExp of val (* value *)
   | OpExp of {l: expression; op: operator; r: expression} (* operator *)
   | SeqExp of expression list (* sequence *)
-  (*| lock *)
+  | LockExp of {label: string; body: expression} (* lock *)
   (*| callexp *)
-  (*| if *)
+  | IfExp of {cond: expression; then_exp: expression; else_exp: expression} (* if *)
   (*| assignment - will require a variable environment to make sense *)
-  (*| let - same as above *)
-  
+  | LetExp of {var_name: string; var_exp: expression; body_exp: expression} (* let *)
+
+type parse_tree =
+| ValueNode of val
+| OperatorNode of {left: parse_tree; operator: operator; right: parse_tree}
+| SequenceNode of parse_tree list
+| LockNode of {label: string; body: parse_tree}
+| IfNode of {condition: parse_tree; then_branch: parse_tree; else_branch: parse_tree}
+
+
+let parse_value (v: value) : value =
+  match v with
+  | Int i -> Int i
+  | Bool b -> Bool b
+
+let parse_if (cond: expression) (then_exp: expression) (else_exp: expression) : parse_tree =
+  let parsed_cond = parse_exp cond in
+  let parsed_then = parse_exp then_exp in
+  let parsed_else = parse_exp else_exp in
+  IfNode {condition = parsed_cond; then_branch = parsed_then; else_branch = parsed_else}
+  (* Construct appropriate parse tree node for if expression *)
+  (* raise NotImplemented *)
+
+  let parse_let (var_name: string) (var_exp: expression) (body_exp: expression) : parse_tree =
+    let parsed_var_exp = parse_exp var_exp in
+    let parsed_body_exp = parse_exp body_exp in
+    (* Logic to handle let expression, Need assignment *)
+    raise NotImplemented  
+
+
 
 (* Rather than make statements, we essentially make due with expressions. *)
 (* So, we move directly onto configuration *)
@@ -42,9 +70,17 @@ type config =
 let rec parse (expression: expression) (c: config) : parse_tree = 
   let rec parse_exp (e: expression) : exp =
     match e with
-    | ValExp v -> raise NotImplemented
+    | ValExp v -> ValueNode (parse_value v)
     | OpExp l op r -> raise NotImplemented
     | SeqExp exp_list -> raise NotImplemented
+    | LockExp {label; body} ->
+    let parsed_label = parse_exp label in
+    let parsed_body = parse_exp body in
+    (* Logic to handle lock expression *)
+    raise NotImplemented
+    | IfExp {cond; then_exp; else_exp} -> parse_if cond then_exp else_exp
+    | LetExp {var_name; var_exp; body_exp} -> parse_let var_name var_exp body_exp
+  
   in parse_exp expression
 
 
