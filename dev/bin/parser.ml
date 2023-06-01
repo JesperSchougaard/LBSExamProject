4934c9a8510df6d4aefcbe4c6c8d1a3eb04b87e2
Oliver <domolitom@gmail.com>
2023-06-01 16:59:29 +0200

    small changes in the types and parser

--- dev/bin/parser.ml
+++ dev/bin/parser.ml
@@ -24,16 +24,20 @@ let parse_if (cond: T.expression) (then_exp: T.expression) (else_exp: T.expressi
   (* raise NotImplemented *)
 
 let parse_let (var_name: string) (var_exp: T.expression) (body_exp: T.expression) : T.parse_tree =
-  let parsed_var_exp = parse_exp var_exp in
-  let parsed_body_exp = parse_exp body_exp in
+  let parsed_var_exp = parse_expression var_exp in
+  let parsed_body_exp = parse_expression body_exp in
+  T.Let_node { var_name = var_name; var_exp = parsed_var_exp; body_exp = parsed_body_exp }
+
+let parse_lock (label: string) (body: T.expression) : T.parse_tree =
+  let parsed_body = parse_expression body in
+  T.Lock_node { label = label; body = parsed_body }
 
 let rec parse_seq_exp expressions =
   match expressions with
-  | [] -> () (* Base case: Empty sequence *)
+  | [] -> Unit_node
   | exp :: rest ->
-    let _ = parse_exp exp in (* Evaluate and process the current expression *)
-    parse_seq_exp rest (* Recursively process the remaining expressions in the sequence *)
-
+    let _ = parse_exp exp in
+    parse_seq_exp rest
 
 (* Rather than make statements, we essentially make due with expressions. *)
 (* So, we move directly onto configuration *)
@@ -52,14 +56,10 @@ let rec parse (expression: T.expression) (c: config) : T.parse_tree =
     | ValExp v -> ValueNode("bad", parse_value v)
     | OpExp l op r -> raise NotImplemented
     | SeqExp exp_list -> raise NotImplemented
-    | LockExp {label; body} ->
-    let parsed_label = parse_exp label in
-    let parsed_body = parse_exp body in
-    (* Logic to handle lock expression *)
-    raise NotImplemented
+    | LockExp {label; body} -> parse_lock label body
     | IfExp {cond; then_exp; else_exp} -> parse_if cond then_exp else_exp
     | LetExp {var_name; var_exp; body_exp} -> parse_let var_name var_exp body_exp
-    | CallExp {func_exp; args}
+    | CallExp {func_exp; args} -> T.Call_node func_exp args
   
   in parse_exp expression
 
--- dev/bin/typing.ml
+++ dev/bin/typing.ml
@@ -2,14 +2,14 @@
 
 module T = Types
 
-exception NotImplemeneted
+exception NotImplemented
 
-(* Define types needed  *)
+(* Define types needed *)
 
 (* Type checker gets as input a variable environment, two labels, and a parse_tree *)
 (* If all typed sub-expressions/programs evaluate to true, then the entire program is true *)
 let rec type_checker 
-  (gamma: map:string -> T.types) (pc: string)
+  (gamma: string -> T.types) (pc: string)
   (lambda_input: string) (lambda_output: string) 
   (pt: T.parse_tree) : bool =
   match pt with
@@ -18,10 +18,7 @@ let rec type_checker
   | Sequence_node list -> true
   | Lock_node {label; body} -> true
   | If_node {condition; then_branch; else_branch} -> true
-
-
+  | Call_node {func_name; args} -> true
 
 (* random test function *)
 let typinghello = print_endline "\n typing says hello!"
-
-
